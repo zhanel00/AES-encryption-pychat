b0d33cbd24ea0a0cdbc68ebe265a8c7db4734e05
Erdauit Torekhan <98638156+Erdauit@users.noreply.github.com>
2024-02-29 02:54:26 +0600

    added service for chatting

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+from aes_cipher import SimpleCipher
+from AES import AES
+
+HOST = '127.0.0.1'
+PORT = 65432
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+nickname = input("Choose your nickname: ")
+password = input("Please write your secret password between your friends")
+
+cipher = AES(password, 128)
+def receive():
+    while True:
+        try:
+            message = client.recv(1024)
+            decrypted_message = cipher.decrypt_text(message)
+            print(f"Decrypted message: {decrypted_message}")
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        encrypted_message = cipher.encrypt_text(message)
+        print(f"Encrypted message: {encrypted_message}")
+        client.send(encrypted_message)
+
+threading.Thread(target=receive).start()
+threading.Thread(target=write).start()
---
+++ server.py
@@ -0,0 +1,38 @@
+import socket
+import threading
+from aes_cipher import SimpleCipher
+
+HOST = '127.0.0.1'
+PORT = 65432
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+clients = []
+key = 'mysecretkey'
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"Received encrypted message: {message}")
+            broadcast(message)
+        except:
+            clients.remove(client)
+            client.close()
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+        clients.append(client)
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("Server is listening...")
+receive()
